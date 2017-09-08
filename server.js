86fe15d4dc5b56f8c1f1deff7e0dd75c547408f7
Joseph Fan <joefan@joe-fan.com>
2017-09-08 17:26:03 -0600

    adding ssl cert

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
 var express     = require('express')
 var app         = express()
+var HTTP = require('http')
+var HTTPS = require('https')
+var fs = require('fs')
+
 app.use(express.static('./'))
+
+try {
+    var httpsConfig = {
+        key  : fs.readFileSync('/etc/letsencrypt/live/dev.curatingme.com/privkey.pem'),
+        cert : fs.readFileSync('/etc/letsencrypt/live/dev.curatingme.com/cert.pem')
+    }
+    var httpsServer = HTTPS.createServer(httpsConfig, app)
+    httpsServer.listen(443)
+}
+catch(error){
+    console.log(error)
+    console.log('could not set up HTTPS')
+}
+finally {
+    console.log('this code runs regardless of whether the above code succeeded or failed')
+}
+
+var httpApp = express()
+httpApp.use(function(req, res){
+    console.log(req.url)
+    res.redirect('https://thepasswordisdragons.com' + req.url)
+})
+
 app.listen(80)
