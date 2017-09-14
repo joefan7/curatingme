1681fe242939ef57d2254b3ee0ac087009dbcacd
Joseph Fan <joefan@joe-fan.com>
2017-09-13 23:07:46 -0600

    authentication

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var HTTP = require('http');
-var HTTPS = require('https'); 
-var fs = require('fs');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+var HTTP = require('http');
+var HTTPS = require('https'); 
+var fs = require('fs');
 
 //ssl
 var httpApp = express()
@@ -38,7 +38,7 @@ httpApp.use(function(req, res){
 })
 
 //connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/curatingMe');
+mongoose.connect('mongodb://localhost/curatingMe');
 var db = mongoose.connection;
 
 //handle mongo error
@@ -49,17 +49,18 @@ db.once('open', function () {
 
 //use sessions for tracking logins
 app.use(session({
-    secret: 'sljfir dajhjed',
-    resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({
-      mongooseConnection: db
-    })
-  }));
+  secret: 'adoiuend ahsudi',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 
 // parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 
 // serve static files from template
 app.use(express.static(__dirname + '/template'));
@@ -70,20 +71,20 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('File Not Found');
-    err.status = 404;
-    next(err);
-  });
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 // define as the last app.use callback
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.send(err.message);
-  });
-  
-  
-  // listen on port 80
-  app.listen(80, function () {
-    console.log('Express app listening on port 3000');
-  });
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+// listen on port 8000
+app.listen(80, function () {
+  console.log('Express app listening on port 80');
+});
