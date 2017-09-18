4b81211f8ad059d254ff938f93b2f106517d5647
Joseph Fan <joefan@joe-fan.com>
2017-09-18 10:09:13 -0600

    code restart

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+var gUserId = ''
+var gUserName = ''
+
+window.fbAsyncInit = function () {
+    FB.init({
+      appId: '1526244437434268',
+      cookie: true,
+      xfbml: true,
+      version: 'v2.8'
+    });
+    FB.getLoginStatus(function (response) {
+      statusChangeCallback(response);
+    });
+    FB.AppEvents.logPageView(); 
+  };
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
