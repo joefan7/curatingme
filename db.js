ef989c0d2cd46baf558160b0e55a24bbf6d55b2e
Joseph Fan <joefan@joe-fan.com>
2017-09-18 17:35:47 -0600

    FB Login 18

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/curatingme')
+mongoose.connect('mongodb://localhost:27017/curatingme')
 
 var userInformationSchema = mongoose.Schema({
     userId: {
