a2ea78a610efb11fee275d335e22f7c1c2eea7c1
Joseph Fan <joefan@joe-fan.com>
2017-09-19 17:48:06 -0600

    UserInformation load

--- public/js/main.js
+++ public/js/main.js
@@ -125,12 +125,6 @@ function buildLoginPrompt() {
     let loginPrompt = `
     <h1>Welcome to CuratingMe.com</h1>
     <img src="/images/logo.png" class="img-responsive img-center">
-    <ol class="numeric">
-        <li>Add Links</li>
-        <li>Create Lists</li>
-        <li>Generate Page</li>
-        <li>Copy Page Links</li>      
-    </ol>
   `;
     document.getElementById('user-input-area').innerHTML = loginPrompt;
 }
