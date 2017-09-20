a063381408d2a72186b3d34e637e216eb1d81fab
Joseph Fan <joefan@joe-fan.com>
2017-09-20 00:28:17 -0600

    UserInformation load

--- public/js/main.js
+++ public/js/main.js
@@ -85,7 +85,8 @@ function buildProfileInput(dataFromUserCall) {
                 <form id="uiForm">
                     <div class="form-group text-left">
                         <h2>${gUserName}, thank you for logging in.</h2>
-                        <a href="/instructions">Start Curating</a>
+                        <p>If this is your first time here, please visit the <a href="/instructions">instructions</a> page</p> 
+                        
                     </div>
                 </form>
             </div>
