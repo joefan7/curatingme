5bf7bfed44d47c803461278e8a02c2ca3ec35c82
Joseph Fan <joefan@joe-fan.com>
2017-09-21 04:29:14 -0600

    Manage Lists

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,65 +2,92 @@ $(document).ready(function () {
     $('.footer-template').load("./html/footer.html");
     console.log("Local Storage _id:", localStorage._id);
 
-    var render = function () {
-        $('#manage-links-list').empty();
+    var renderLinks = function () {
+        $('#links-list').empty();
         for (var i = 0; i < linkList.length; i++) {
-            $('#manage-links-list').append(`
-            <li id="${linkList[i]._id}" class="link">
-                <button btn-link-number="${linkList[i]._id}" class="linkButton">Delete</button>
-                ${linkList[i]['linkName']}
-                -
-                ${linkList[i]['linkUrl']}
-            </li>
+            $('#links-list').append
+                (`
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="links[]" value="${linkList[i].linkName} - ${linkList[i].linkUrl}">${linkList[i].linkName} - ${linkList[i].linkUrl}
+                    </label>
+                </div>
             `);
         }
     };
-    
-    // <button btn-link-number="${linkList[i]._id}" class="testButton">Test</button>
-
-    // var getFreshData = function () {
-    //     $.get('/linkList', $(this).serialize() + 'objId=' + localStorage._id, function (linkData) {
-    //         linkList = linkData;
-    //         render();
-    //     });
-    // };
+    var renderLists = function () {
+        console.log("listList: ", listList);
+        // clear out lists-list and lists-list-links
+        $('#lists-list').empty();
+        $('#lists-list-links').empty();
+        $('input:checkbox').removeAttr('checked');    
+        for (var i = 0; i < listList.length; i++) {
+            $('#lists-list').append
+                (`
+                <li id="${listList[i]._id}" class="list">
+                <button btn-list-number="${listList[i]._id}" class="listButton">Delete List</button>
+                ${listList[i]['listName']}
+                <br>
+                ${listList[i]['listLinks']} <p>   </p>
+                </li>
+                `);
+        }
+    };
 
     var getFreshData = function () {
-        $.get('/linkList', function (linkData) {
+        // get list of links for check box group
+        $.get('/list/linkList', function (linkData) {
             linkList = linkData;
-            render();
+            renderLinks();
+        });
+        // get list of lists and lookup links from link collection
+        $.get('/listList', function (listData) {
+            listList = listData;
+            renderLists();
         });
     };
-    
+
     // collapse nav bar when selection made
     $('.navbar-nav>li>a').on('click', function () {
         $('.navbar-collapse').collapse('hide');
     });
 
     var linkList = [];
+    var listList = [];
+    var checkedArr = [];
     getFreshData();
 
-    $('body').on('click', '.linkButton', function (event) {
+    $('body').on('click', '.listButton', function (event) {
         event.stopPropagation();
         event.preventDefault();
-        var btnLinkNumber = $(event.target).attr('btn-link-number');
-        var btnItem = document.getElementById(btnLinkNumber);
-        console.log("Button Clicked", btnLinkNumber, btnItem);
-        $.post('/linkList/delete', $(this).serialize() + '_id=' + btnLinkNumber, function (linkData) {
+        var btnListNumber = $(event.target).attr('btn-list-number');
+        var btnItem = document.getElementById(btnListNumber);
+        console.log("Button Clicked", btnListNumber, btnItem);
+        $.post('/listList/delete', $(this).serialize() + '_id=' + btnListNumber, function (listData) {
             getFreshData();
         });
     });
 
-    $('#manage-links-section').on('submit', function (event) {
+    $('#manage-lists-section').on('submit', function (event) {
         event.preventDefault();
-        $.post('/linkList', {
-            objId: localStorage._id,
-            linkName: $('#linkName').val(),
-            linkUrl: $('#linkUrl').val()
+        $('input[type=checkbox]:checked').each(function () {
+            checkedArr.push($(this).val());
         });
-        document.getElementById('linkUrl').value = '';
-        document.getElementById('linkName').value = '';
-        getFreshData();
+        if (checkedArr.length === 0) {
+            alert("You must check at least one link.")
+        } else {
+            console.log("Checked items array", checkedArr);
+            console.log("objId of user", localStorage._id);
+            console.log("List name", $('#listName').val());
+            $.post('/listList', {
+                objId: localStorage._id,
+                listName: $('#listName').val(),
+                listLinks: checkedArr.join()
+            });
+            document.getElementById('listName').value = '';
+            checkedArr = [];
+            getFreshData();
+        };
     });
 });
 
@@ -85,7 +112,8 @@ window.fbAsyncInit = function () {
         version: 'v2.8'
     });
     FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
+        setElements(true);
+        // statusChangeCallback(response);
     });
     FB.AppEvents.logPageView();
 };
@@ -173,7 +201,7 @@ $(document).on('click', '#build', function (evt) {
 
 // Build the Login Prompt
 function buildLoginPrompt() {
-    alert(`${gUserName}, you are now logged out.`);
+    alert(`You are now logged out.`);
     window.location.href = "https://www.curatingme.com";
     location.reload();
 }
@@ -188,14 +216,14 @@ function checkLoginState() {
 // Toggle visibility of screen elements if logged in
 function setElements(isLoggedIn) {
     if (isLoggedIn) {
-        document.getElementById('manage-links-section').style.display = 'block';
+        document.getElementById('manage-lists-section').style.display = 'block';
         document.getElementById('nav-dash').style.display = 'block';
         document.getElementById('nav-links').style.display = 'block';
         document.getElementById('nav-lists').style.display = 'block';
         document.getElementById('logout').style.display = 'block';
         document.getElementById('fb-btn').style.display = 'none';
     } else {
-        document.getElementById('manage-links-section').style.display = 'none';
+        document.getElementById('manage-lists-section').style.display = 'none';
         document.getElementById('nav-dash').style.display = 'none';
         document.getElementById('nav-links').style.display = 'none';
         document.getElementById('nav-lists').style.display = 'none';
@@ -208,11 +236,11 @@ logout = function () {
     FB.logout(function (response) {
         buildLoginPrompt();
         setElements(false);
+        setTimeout(window.location.href = "https://curatingme.com", 5000);
     });
 }
 
 // onclick event is assigned to the #button element.
 document.getElementById("logout").onclick = function () {
     logout();
-    window.location.href = "https://curatingme.com";
 };
