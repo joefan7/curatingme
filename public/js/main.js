b2727998d1c4913999441e59e77347f2e50c4b32
Joseph Fan <joefan@joe-fan.com>
2017-09-21 05:10:13 -0600

    Manage Lists

--- public/js/main.js
+++ public/js/main.js
@@ -87,8 +87,10 @@ function buildProfileInput(dataFromUserCall) {
                     <div class="form-group text-left">
                         <h2>${gUserName}, thank you for logging in.</h2>
                         <hr>
+                        <h4>
                         <p>If this is your first time here, please visit the <a href="/instructions">instructions</a> page.</p>
                         <p>If you have any issues please don't hesitate to contact us at <a href="mailto:support@curatingme.com">support@curatingme.com</a>.</p> 
+                        </h4>
                         <hr>
                     </div>
                 </form>
