dd355418877e3c7789e5933b8d61a131d7e98419
Joseph Fan <joefan@joe-fan.com>
2017-09-21 01:44:33 -0600

    Manage Lists

--- public/js/manage-lists.js
+++ public/js/manage-lists.js
@@ -28,141 +28,141 @@ $(document).ready(function () {
                 ${listList[i]['listName']}
                 </li>
                 `);
-             console.log("INDEX: ",listList[i]['listObjIds']);   
+            console.log("INDEX: ", listList[i]['listObjIds']);
             // for (var index in listList[i]['listObjIds']){
             //     $.get('/link', {_id: listList[i]['listObjIds']}, function (linkData){
             //         console.log("dataFromServer", linkData); // apend next
-                // });
-            }
-        };
+            // });
+        }
     };
+});
 
-    var getFreshData = function () {
-        // get list of links for check box group
-        $.get('/list/linkList', function (linkData) {
-            linkList = linkData;
-            renderLinks();
-        });
-        // get list of lists and lookup links from link collection
-        $.get('/listList', function (listData){
-            listList = listData;
-            renderLists();
-        });
-    };
-
-    // collapse nav bar when selection made
-    $('.navbar-nav>li>a').on('click', function () {
-        $('.navbar-collapse').collapse('hide');
+var getFreshData = function () {
+    // get list of links for check box group
+    $.get('/list/linkList', function (linkData) {
+        linkList = linkData;
+        renderLinks();
     });
-
-    var linkList = [];
-    var listList = [];
-    var checkedArr = [];
-    getFreshData();
-
-    $('body').on('click', '.listButton', function (event) {
-        event.stopPropagation();
-        event.preventDefault();
-        var btnListNumber = $(event.target).attr('btn-list-number');
-        var btnItem = document.getElementById(btnListNumber);
-        console.log("Button Clicked", btnListNumber, btnItem);
-        $.post('/listList/delete', $(this).serialize() + '_id=' + btnListNumber, function (listData) {
-            getFreshData();
-        });
+    // get list of lists and lookup links from link collection
+    $.get('/listList', function (listData) {
+        listList = listData;
+        renderLists();
     });
+};
 
-    $('#manage-lists-section').on('submit', function (event) {
-        event.preventDefault();
-        $('input[type=checkbox]:checked').each(function () {
-            checkedArr.push($(this).val());
-        });
-        if (checkedArr.length === 0) {
-            alert("You must check at least one link.")
-        } else {
-            console.log("Checked items array", checkedArr);
-            console.log("objId of user", localStorage._id);
-            console.log("List name", $('#listName').val());
-            $.post('/listList', {
-                objId: localStorage._id,
-                listName: $('#listName').val(),
-                listObjIds: checkedArr.join()
-            });
-            document.getElementById('listName').value = '';
-            getFreshData();
-        };
+// collapse nav bar when selection made
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+var linkList = [];
+var listList = [];
+var checkedArr = [];
+getFreshData();
+
+$('body').on('click', '.listButton', function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    var btnListNumber = $(event.target).attr('btn-list-number');
+    var btnItem = document.getElementById(btnListNumber);
+    console.log("Button Clicked", btnListNumber, btnItem);
+    $.post('/listList/delete', $(this).serialize() + '_id=' + btnListNumber, function (listData) {
+        getFreshData();
     });
 });
 
-
-    var gUserId = '';
-    var gUserName = '';
-    var gUserEmail = '';
-    // Get User Information
-    var getUserInformation = function (userId) {
-        $.get('/userInformation', { userId: userId }, function (dataFromServer) {
-            buildProfileInput(dataFromServer);
+$('#manage-lists-section').on('submit', function (event) {
+    event.preventDefault();
+    $('input[type=checkbox]:checked').each(function () {
+        checkedArr.push($(this).val());
+    });
+    if (checkedArr.length === 0) {
+        alert("You must check at least one link.")
+    } else {
+        console.log("Checked items array", checkedArr);
+        console.log("objId of user", localStorage._id);
+        console.log("List name", $('#listName').val());
+        $.post('/listList', {
+            objId: localStorage._id,
+            listName: $('#listName').val(),
+            listObjIds: checkedArr.join()
         });
+        document.getElementById('listName').value = '';
+        getFreshData();
     };
+});
+});
 
-    // FB Login Initialization
-    window.fbAsyncInit = function () {
-        console.log("fbAsyncInit");
-        FB.init({
-            appId: '954902444648707',
-            cookie: true,
-            xfbml: true,
-            version: 'v2.8'
-        });
-        FB.getLoginStatus(function (response) {
-            setElements(true);
-            // statusChangeCallback(response);
-        });
-        FB.AppEvents.logPageView();
-    };
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
 
-    // Check status and run appropriate
-    function statusChangeCallback(response) {
-        if (response.status === 'connected') {
-            console.log('Logged in and authenticated', response.status);
-            setElements(true);
-            testAPI();
-        } else {
-            console.log('Not authenticated', response.status);
-            setElements(false);
+var gUserId = '';
+var gUserName = '';
+var gUserEmail = '';
+// Get User Information
+var getUserInformation = function (userId) {
+    $.get('/userInformation', { userId: userId }, function (dataFromServer) {
+        buildProfileInput(dataFromServer);
+    });
+};
+
+// FB Login Initialization
+window.fbAsyncInit = function () {
+    console.log("fbAsyncInit");
+    FB.init({
+        appId: '954902444648707',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.8'
+    });
+    FB.getLoginStatus(function (response) {
+        setElements(true);
+        // statusChangeCallback(response);
+    });
+    FB.AppEvents.logPageView();
+};
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Check status and run appropriate
+function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+        console.log('Logged in and authenticated', response.status);
+        setElements(true);
+        testAPI();
+    } else {
+        console.log('Not authenticated', response.status);
+        setElements(false);
+    }
+}
+// Pull in UserName, UserId and Email from FB
+function testAPI() {
+    FB.api('/me?fields=name,email', function (response) {
+        if (response && !response.error) {
+            console.log("RESPONSE", response);
+            gUserName = response.name;
+            gUserId = response.id;
+            gUserEmail = response.email;
+            getUserInformation(response.id);
         }
-    }
-    // Pull in UserName, UserId and Email from FB
-    function testAPI() {
-        FB.api('/me?fields=name,email', function (response) {
-            if (response && !response.error) {
-                console.log("RESPONSE", response);
-                gUserName = response.name;
-                gUserId = response.id;
-                gUserEmail = response.email;
-                getUserInformation(response.id);
-            }
-        })
-    }
+    })
+}
 
-    function buildProfileInput(dataFromUserCall) {
-        console.log("dataFromUserCall", dataFromUserCall.uiName)
-        // if all data fields in dataFromUserCall are populated create a welcome message with links to activity and food entry pages
-        if (
-            (dataFromUserCall !== "") &&
-            (dataFromUserCall.userId !== "") &&
-            (dataFromUserCall.uiName !== "") &&
-            (dataFromUserCall.uiEmail !== "")
-        ) {
-        } else {
-            setElements(false);
-            let userInputForm = `
+function buildProfileInput(dataFromUserCall) {
+    console.log("dataFromUserCall", dataFromUserCall.uiName)
+    // if all data fields in dataFromUserCall are populated create a welcome message with links to activity and food entry pages
+    if (
+        (dataFromUserCall !== "") &&
+        (dataFromUserCall.userId !== "") &&
+        (dataFromUserCall.uiName !== "") &&
+        (dataFromUserCall.uiEmail !== "")
+    ) {
+    } else {
+        setElements(false);
+        let userInputForm = `
       <form id="uiForm">
       <div class="form-group">
           <h2>Thank you for logging in, ${gUserName}.</h2>
@@ -175,74 +175,74 @@ $(document).ready(function () {
       </div>
       </form>
       `;
-            document.getElementById('user-input-area').innerHTML = userInputForm;
-        }
+        document.getElementById('user-input-area').innerHTML = userInputForm;
     }
-    $(document).on('click', '#build', function (evt) {
-        evt.preventDefault();
-        // Create User Information Doc
-        $.post('/user_information/create', {
-            userId: $('#userId').val(),
-            uiName: $('#uiName').val(),
-            uiEmail: $('#uiEmail').val()
-        }, function (dataFromServer) {
-            console.log("dataFromServer : ", dataFromServer)
-            buildProfileInput(dataFromServer);
-        })
-        let userInputForm = `
+}
+$(document).on('click', '#build', function (evt) {
+    evt.preventDefault();
+    // Create User Information Doc
+    $.post('/user_information/create', {
+        userId: $('#userId').val(),
+        uiName: $('#uiName').val(),
+        uiEmail: $('#uiEmail').val()
+    }, function (dataFromServer) {
+        console.log("dataFromServer : ", dataFromServer)
+        buildProfileInput(dataFromServer);
+    })
+    let userInputForm = `
           <form id="uiForm">
           <div class="form-group">
               <h2>New User Created</h2>
           </div>
           </form>
           `;
-        document.getElementById('user-input-area').innerHTML = userInputForm;
-        setElements(true);
-        setTimeout(location.reload.bind(location), 2000);
+    document.getElementById('user-input-area').innerHTML = userInputForm;
+    setElements(true);
+    setTimeout(location.reload.bind(location), 2000);
+});
+
+// Build the Login Prompt
+function buildLoginPrompt() {
+    alert(`You are now logged out.`);
+    window.location.href = "https://www.curatingme.com";
+    location.reload();
+}
+
+// Check FB Login State
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
     });
+}
 
-    // Build the Login Prompt
-    function buildLoginPrompt() {
-        alert(`You are now logged out.`);
-        window.location.href = "https://www.curatingme.com";
-        location.reload();
+// Toggle visibility of screen elements if logged in
+function setElements(isLoggedIn) {
+    if (isLoggedIn) {
+        document.getElementById('manage-lists-section').style.display = 'block';
+        document.getElementById('nav-dash').style.display = 'block';
+        document.getElementById('nav-links').style.display = 'block';
+        document.getElementById('nav-lists').style.display = 'block';
+        document.getElementById('logout').style.display = 'block';
+        document.getElementById('fb-btn').style.display = 'none';
+    } else {
+        document.getElementById('manage-lists-section').style.display = 'none';
+        document.getElementById('nav-dash').style.display = 'none';
+        document.getElementById('nav-links').style.display = 'none';
+        document.getElementById('nav-lists').style.display = 'none';
+        document.getElementById('logout').style.display = 'none';
+        document.getElementById('fb-btn').style.display = 'block';
     }
+}
 
-    // Check FB Login State
-    function checkLoginState() {
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
-        });
-    }
+logout = function () {
+    FB.logout(function (response) {
+        buildLoginPrompt();
+        setElements(false);
+        setTimeout(window.location.href = "https://curatingme.com", 5000);
+    });
+}
 
-    // Toggle visibility of screen elements if logged in
-    function setElements(isLoggedIn) {
-        if (isLoggedIn) {
-            document.getElementById('manage-lists-section').style.display = 'block';
-            document.getElementById('nav-dash').style.display = 'block';
-            document.getElementById('nav-links').style.display = 'block';
-            document.getElementById('nav-lists').style.display = 'block';
-            document.getElementById('logout').style.display = 'block';
-            document.getElementById('fb-btn').style.display = 'none';
-        } else {
-            document.getElementById('manage-lists-section').style.display = 'none';
-            document.getElementById('nav-dash').style.display = 'none';
-            document.getElementById('nav-links').style.display = 'none';
-            document.getElementById('nav-lists').style.display = 'none';
-            document.getElementById('logout').style.display = 'none';
-            document.getElementById('fb-btn').style.display = 'block';
-        }
-    }
-
-    logout = function () {
-        FB.logout(function (response) {
-            buildLoginPrompt();
-            setElements(false);
-            setTimeout(window.location.href = "https://curatingme.com", 5000);
-        });
-    }
-
-    // onclick event is assigned to the #button element.
-    document.getElementById("logout").onclick = function () {
-        logout();
-    };
+// onclick event is assigned to the #button element.
+document.getElementById("logout").onclick = function () {
+    logout();
+};
